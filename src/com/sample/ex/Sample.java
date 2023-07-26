090d754e5460110a00c6ef210e3896e613339ca8
Merin <merintvarghese19@gmail.com>
2023-07-26 12:34:02 -0700

    New Changes sample commit

--- src/com/sample/ex/Sample.java
+++ src/com/sample/ex/Sample.java
@@ -2,6 +2,6 @@ package com.sample.ex;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World..!!");
     }
 }
